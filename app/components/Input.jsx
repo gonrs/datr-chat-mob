ba68ebf1dce2b3fc8c73b61d2a2baa2e680c1098
Gonr <one642077@gmail.com>
2023-05-21 18:19:49 +0300

    files

---
+++ app/components/Input.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Input() {
+  return (
+    <View>
+      <Text>Input</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/page/Logis.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Logis = () => {
+	return (
+		<View>
+			<Text>Logis</Text>
+		</View>
+	)
+}
+
+export default Logis
+
+const styles = StyleSheet.create({})
