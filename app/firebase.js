52bd8318c6158c3baf6c8ed129c782fcfde54be2
Gonr <one642077@gmail.com>
2023-06-09 10:44:45 +0300

    3.0.1 ebus

--- app/firebase.js
+++ app/firebase.js
@@ -2,15 +2,24 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
+import {
+	REACT_APP_API_KEY,
+	REACT_APP_AUTH_DOMAIN,
+	REACT_APP_PROJECT_ID,
+	REACT_APP_STORAGE_BUCKET,
+	REACT_APP_SENDER_ID,
+	REACT_APP_APP_ID,
+	REACT_APP_MEASUREMENT_ID,
+} from 'react-native-dotenv'
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+	apiKey: REACT_APP_API_KEY,
+	authDomain: REACT_APP_AUTH_DOMAIN,
+	projectId: REACT_APP_PROJECT_ID,
+	storageBucket: REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: REACT_APP_SENDER_ID,
+	appId: REACT_APP_APP_ID,
+	measurementId: REACT_APP_MEASUREMENT_ID,
 }
 console.log(process.env.REACT_APP_API_KEY)
 export const app = initializeApp(firebaseConfig)
